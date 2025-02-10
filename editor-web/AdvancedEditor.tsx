34b3d689acac3c3d0e33c5e85e3eeab399628d12
Benaiah Barango <benaiah.a.barango@gmail.com>
2025-02-09 21:32:23 -0500

    working counter bridge

--- editor-web/AdvancedEditor.tsx
+++ editor-web/AdvancedEditor.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { EditorContent } from "@tiptap/react";
-import { useTenTap, CoreBridge } from "@10play/tentap-editor";
+import { useTenTap, CoreBridge, TenTapStartKit } from "@10play/tentap-editor";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
+
+import { CounterBridge } from "../components/CounterBridge";
 
 /**
  * Here we control the web side of our custom editor
  */
 export const AdvancedEditor = () => {
   const editor = useTenTap({
-    bridges: [CoreBridge],
+    bridges: [CoreBridge, CounterBridge, ...TenTapStartKit],
     tiptapOptions: {
       extensions: [Document, Paragraph, Text],
     },
